92e67092d409100e765a4bdc15b9af18504de2c9
Allyn Bauer <discobeetle.software@gmail.com>
2020-10-08 12:48:22 -0500

    Remove unused code

--- src/renderer.rb
+++ src/renderer.rb
@@ -70,7 +70,7 @@ class Renderer
 	end
 	
 	protected
-	
+
 	def parsed_options?
 		opts = OptionParser.new
 		opts.on('-v', '--version') { output_version; exit }
@@ -82,73 +82,19 @@ class Renderer
 	
 	def output_help
 		output_version
-		# puts RDoc::usage
-	end
-	
-	def output_usage
-		# RDoc::usage('usage')
+		puts "ruby build.rb"
 	end
 	
 	def output_version
 		puts "#{File.basename(__FILE__)} version #{VERSION}"
 	end
 	
-
 	private
 	
 	def render
 		ERB.new(@template).result(binding)
 	end
 
-	def each_gallery_item
-		# Dir[File.join(@options.input, '**', "*.*")].each do |path|
-		# 	key = DownieResult.name(path)
-		# 	yield path, key
-		# end
-	end
-
-	def load
-		# candidates = {}
-		# each_gallery_item do |path, key|
-		# 	element = candidates[key]
-		# 	if element.nil?
-		# 		element = DownieResult.new(path, @options.input)
-		# 		candidates[element.name] = element
-		# 	else
-		# 		element.append(path)
-		# 	end
-# 		end
-# 
-# 		result = candidates.values
-# 		result = result.select(&:has_result)
-# 		result.each(&:populate_json)
-# 		result.sort_by(&:name)
-	end
-
-	def has_invalid_input
-		@options.input.nil?
-	end
-
-	def has_invalid_output
-		@options.output.nil?
-	end
-
-	def configure_environment
-		if has_invalid_input
-			helper = DownieHelpers.new
-			@options.input = helper.download_folder_path
-		end
-		if has_invalid_output
-			@options.output = File.expand_path(File.join("~", "Desktop", DEFAULT_OUTPUT_FILE))
-		end
-		@source = File.expand_path(File.join(__dir__, '..'))
-		def append_includes(*files)
-			files.collect { |file| File.join(@source, 'includes', file) }
-		end
-		@stylesheets = append_includes('DataTables/datatables.min.css')
-		@scripts = append_includes('DataTables/datatables.min.js', 'DataTables/Moment-2.8.4/moment.min.js', 'DataTables/Moment-2.8.4/datetime-moment.js')
-	end
-	
 	def execute
 		File.open(@options.output, 'w') do |f|
 			f.write(render)
@@ -157,5 +103,5 @@ class Renderer
 end
 
 if __FILE__ == $0
-	puts("Loaded #{GalleryRenderer}")
+	puts("Loaded #{__FILE__}")
 end
